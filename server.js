e919db741398c7eb5d98eb8bf91bcb804d462347
padulam <padula.matthew@gmail.com>
2016-10-01 16:39:34 -0400

    Initial app commit

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static('public'));
+
+app.get('/:date',function(request,response){
+	response.json(convertDate(request.params.date))
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+function checkForUnixDate(d){
+	if(isNaN(+d)){
+		return false;
+	}
+	return true;
+}
+
+function convertDate(d){
+	if(checkForUnixDate(d)){
+		d*=1000;
+	}
+	var cDate = new Date(d);
+	if(isNaN(cDate)){
+		return {unix: null, natural: null};
+	} else{
+		var unixDate = cDate.getTime()/1000;
+		var natLangDate = cNatLangDate(cDate);
+		return {unix:unixDate,natural: natLangDate};
+	}
+}
+
+function cNatLangDate(d){
+	var months = ["January", "February", "March",
+				"April", "May", "June","July",
+				"August","September","October",
+				"November","December"];
+
+	var month = months[d.getUTCMonth()];
+	var year = d.getUTCFullYear();
+	var date = d.getUTCDate();
+	return month + " " + date + ", " + year;
+}
+
+app.listen(3000);
